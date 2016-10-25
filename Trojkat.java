52163eb253057e8b99473917f2a33cdb6c7fb185
Adam <adam@ak.lenovo>
2016-10-25 21:59:51 +0200

    triangle

---
+++ Trojkat.java
@@ -0,0 +1,20 @@
+/**
+ * Created by adam on 25.10.16.
+ */
+public class Trojkat {
+    public static void main(String[] args){
+            int a = 5;
+            int h = 3;
+
+            double triangle = (double)a*h/2;
+            if(triangle>7){
+                System.out.println("Duzy trojkat");
+            }
+            else{
+                System.out.println("Maly trojkat");
+            }
+
+        }
+
+
+}
