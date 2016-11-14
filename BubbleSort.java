c49a5fe3e04dc6533a24cb1b850656be20a69dee
galileoF <adam@ak.lenovo>
2016-11-14 21:42:57 +0100

    bugs to fix

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,20 +4,41 @@
 public class BubbleSort {
     public static void main(String[] args){
         int[] numbers = new int[] {4, 2, 0, 2, 10, 1, 9, 12, 3, 5};
-        for(int i=0; i < numbers.length; i++){
-            for(int j=0; j<numbers.length-1;j++){
-                if(numbers[j]>numbers[j+1]){
-                    int temp;
-                    temp = numbers[j+1];
-                    numbers[j+1]=numbers[j];
-                    numbers[j]=temp;
-                }
 
+        printNumbers(numbers);
+    }
+    public static void swap(int[] valuesInArray){
+        for(int j=0; j<valuesInArray.length-1;j++) {
+            int temp;
+            temp = valuesInArray[j + 1];
+            valuesInArray[j + 1] = valuesInArray[j];
+            valuesInArray[j] = temp;
+
+
+        }
+
+    }
+    public static boolean isGreaterThan(int[] values){
+        for(int j=0; j<values.length-1;j++) {
+            if (values[j] > values[j + 1]) {
 
             }
         }
-        for(int i=0;i<numbers.length;i++){
-            System.out.println(numbers[i]);
+        return true;
+
+    }
+
+    public static void sort(int[] mess){
+        if(isGreaterThan(mess)){
+            swap(mess);
+
+        }
+    }
+    public static void printNumbers(int[] sorted) {
+        sort(sorted);
+        for (int i = 0; i < sorted.length; i++) {
+            System.out.println(sorted[i]);
+
         }
     }
 
