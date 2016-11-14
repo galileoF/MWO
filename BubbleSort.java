39e19e281857942ea138a86996d4f0a0d0ba3861
galileoF <adam@ak.lenovo>
2016-11-14 20:43:12 +0100

    initail Bubble sort

---
+++ BubbleSort.java
@@ -0,0 +1,24 @@
+/**
+ * Created by adam on 14.11.16.
+ */
+public class BubbleSort {
+    public static void main(String[] args){
+        int[] numbers = new int[] {4, 2, 0, 2, 10, 1, 9, 12, 3, 5};
+        for(int i=0; i < numbers.length; i++){
+            for(int j=0; j<numbers.length-1;j++){
+                if(numbers[j]>numbers[j+1]){
+                    int temp;
+                    temp = numbers[j+1];
+                    numbers[j+1]=numbers[j];
+                    numbers[j]=temp;
+                }
+
+
+            }
+        }
+        for(int i=0;i<numbers.length;i++){
+            System.out.println(numbers[i]);
+        }
+    }
+
+}
