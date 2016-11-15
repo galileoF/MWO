090cc9e32ede9a636959feece23508e6589124e1
galileoF <adam@ak.lenovo>
2016-11-15 22:13:10 +0100

    swap method failed

--- BubbleSort.java
+++ BubbleSort.java
@@ -5,21 +5,21 @@ public class BubbleSort {
     public static void main(String[] args){
         int[] numbers = new int[] {4, 2, 0, 2, 10, 1, 9, 12, 3, 5};
 
+        sort(numbers);
         printNumbers(numbers);
     }
-    public static void swap(int[] values){
-        for(int j=0; j<values.length-1;j++) {
-            int temp;
-            temp = values[j + 1];
-            values[j + 1] = values[j];
-            values[j] = temp;
-
+    public static void swap(int a, int b){
+        int c;
+        c = b;
+        b = a;
+        a = c;
 
         }
 
-    }
-    public static boolean isGreaterThan(int value1, int value2){
-            if (value1 > value2) {
+
+
+    public static boolean isGreaterThan(int a, int b){
+            if (a > b) {
                 return true;
             }
             else {
@@ -29,16 +29,24 @@ public class BubbleSort {
 
     }
 
-    public static void sort(int[] mess){
+    public static int[] sort(int[] mess){
+        for(int i=0;i<mess.length;i++){
         for(int j=0;j<mess.length-1;j++) {
             if (isGreaterThan(mess[j], mess[j+1])) {
-                swap(mess);
-
+                //swap(mess[j], mess[j+1]);
+                int tmp;
+                tmp = mess[j+1];
+                mess[j+1]=mess[j];
+                mess[j]=tmp;
             }
+
         }
+
+    }
+    return mess;
     }
     public static void printNumbers(int[] sorted) {
-        sort(sorted);
+
         for (int i = 0; i < sorted.length; i++) {
             System.out.println(sorted[i]);
 
